a454777f8c2c80dd783bdd1850c998b4f85ef638
Anton Andriichuk <81985737+tishafeed@users.noreply.github.com>
2024-12-27 17:48:00 +0100

    Delete lab2/target directory

--- lab2/target/debug/deps/lab2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\tisha\Desktop\rust-lang\lab2\target\debug\deps\lab2.exe: src/main.rs
-
-C:\Users\tisha\Desktop\rust-lang\lab2\target\debug\deps\lab2.d: src/main.rs
-
-src/main.rs:
--- lab2/target/debug/lab2.d
+++
@@ -1,1 +0,0 @@
-C:\Users\tisha\Desktop\rust-lang\lab2\target\debug\lab2.exe: C:\Users\tisha\Desktop\rust-lang\lab2\src\main.rs
